a6ef7ac58746140e15b05ebc240a40123c0ea7b3
Rotems Computer <Rotem@Compter.com>
2025-11-13 17:17:11 +0200

    add logging to middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,37 +1,43 @@
-/**
- * This middleware handles authentication for page loads (EJS renders).
- * Unlike 'authMiddleware.js' which returns 401/403 JSON errors for APIs,
- * this middleware performs 302 redirects for users.
- */
+import { warn } from "../utils/logger.js";
 
-/**
- * Prevents logged-in users from accessing public pages like /login.
- * If a user is logged in, they are redirected to their feed or profile page.
- */
+// Redirect to feed if authenticated, else proceed
 export function redirectIfAuth(req, res, next) {
   if (req.session.userId) {
     if (req.session.selectedProfileId) {
       return res.redirect("/feed");
     }
+    warn("Profile reqired to access for user", {
+          userId: req.session.userId
+    });
     return res.redirect("/profiles");
   }
   next();
 }
 
+//  Redirect to login if not authenticated, else proceed
 export function requireAuthRedirect(req, res, next) {
   if (!req.session.userId) {
+    warn("Authentication reqires to access", {
+          userId: req.session.userId
+    });
     return res.redirect("/login");
   }
   next();
 }
 
+// Redirect to profiles if no profile selected, else proceed
 export function requireProfileRedirect(req, res, next) {
   if (!req.session.userId) {
+    warn("Access reqires user", {
+          userId: req.session.userId
+    });
     return res.redirect("/login");
   }
   if (!req.session.selectedProfileId) {
+    warn("Access reqires profile", {
+          userId: req.session.userId
+    });
     return res.redirect("/profiles");
   }
-  // Authenticated and profile selected, proceed
   next();
 }
